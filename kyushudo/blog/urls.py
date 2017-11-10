f9dc05eb7eb644d3f13d958c8fe20721534c9cf9
Toshiyuki Yamagami <gamadas@gamadas.jpn.org>
2017-11-10 12:21:18 +0900

    Implemented other templates of views.

--- kyushudo/blog/urls.py
+++ kyushudo/blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     url(r'^$',views.PostListView.as_view(),name='post_list'),
     url(r'^about/',views.AboutView.as_view(),name='about'),
